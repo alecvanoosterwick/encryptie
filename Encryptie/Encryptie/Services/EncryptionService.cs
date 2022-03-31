0768b554092b736a5bf469554daecb830f3ce2f8
Alec Van oosterwijck <r0850108@student.thomasmore.be>
2022-03-31 15:37:01 +0200

    Iets fout in interface, eens laten nakijken

--- Encryptie/Encryptie/Services/EncryptionService.cs
+++ Encryptie/Encryptie/Services/EncryptionService.cs
@@ -1,16 +1,71 @@
-﻿using System;
+﻿using Encryptie.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Encryptie.Services
 {
     public interface IEncryption
     {
-
+        public string Encrypt();
+        public string Decrypt();
     }
     public class EncryptionService : IEncryption
     {
+        private readonly IConfiguration _configuration;
+        private readonly static string key = "1234567891234567";
+        public Bericht bericht = new Bericht();
+        public EncryptionService(IConfiguration configuration)
+        {
 
+        }
+        public string Encrypt()
+        {
+            byte[] iv = new byte[16];
+            byte[] array;
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter streamWriter = new StreamWriter((Stream)cryptoStream))
+                        {
+                            streamWriter.Write(bericht.text);
+                        }
+                        array = ms.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(array);
+        }
+        public string Decrypt()
+        {
+            byte[] iv = new byte[16];
+            byte[] buffer = Convert.FromBase64String(bericht.text);
+            using (Aes aes = Aes.Create())
+            {
+                aes.Key = Encoding.UTF8.GetBytes(key);
+                aes.IV = iv;
+                ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+                using (MemoryStream ms = new MemoryStream(buffer))
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream((Stream)ms, decryptor, CryptoStreamMode.Read))
+                    {
+
+                        using (StreamReader sr = new StreamReader(cryptoStream))
+                        {
+                            return sr.ReadToEnd();
+                        }
+                    }
+                };
+            }
+        }
     }
 }
