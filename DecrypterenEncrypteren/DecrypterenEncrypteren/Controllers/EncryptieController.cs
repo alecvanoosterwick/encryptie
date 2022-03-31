f762d0d97ac2ff5b8f953868d5d6613451069780
Alec Van oosterwijck <r0850108@student.thomasmore.be>
2022-03-31 14:56:49 +0200

    Kan een tekst encrypteren, decrypteren, net Interfece toegevoegd

---
+++ DecrypterenEncrypteren/DecrypterenEncrypteren/Controllers/EncryptieController.cs
@@ -0,0 +1,29 @@
+﻿using DecrypterenEncrypteren.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.HttpGet;
+
+namespace DecrypterenEncrypteren.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncryptieController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("Encrypteren")]
+
+        public string Encrypt(string text)
+        {
+            var encryptString = EncryptieDecryptieManager.Encrypt(text);
+            return encryptString;
+        }
+        [HttpGet]
+        [Route("Decrypteren")]
+        public string Decrypt(string text)
+        {
+            var decryptString = EncryptieDecryptieManager.Decrypt(text);
+            return decryptString;
+        }
+    }
+}
