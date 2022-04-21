b172f25a966cbf2ab42751b608b53f9e832d660f
alecvanoosterwick <r0850108@student.thomasmore.be>
2022-04-21 11:19:29 +0200

    Cleanen code, aanpassen tot het werkt

--- Symmetrische Encryptie/Forge.js
+++ Symmetrische Encryptie/Forge.js
@@ -61,3 +61,24 @@ function Encrypt(){
 
     Encrypt();
     Decrypt();
+
+//werkt maar heeft geen key
+
+// function encrypt() {
+//     var encrypted = CryptoJS.AES.encrypt( //gaat encrypteren op basis van de ingevoerde text en wachtwoord
+//       document.getElementById("text").value,
+//       document.getElementById("password").value,// haalt het wachtwoord op
+//   );
+//     document.getElementById("EncryptedValue").innerHTML = encrypted; // het html bestand met het id krijgt encrypted ingevuld.
+//     document.getElementById("decrypted").innerHTML = ""; //maak decrypted leeg in het geval er iets staat
+//   }
+  
+  
+//   function decrypt() {
+//     var decrypted = CryptoJS.AES.decrypt( //gaat decrypteren op basis van de ingevoerde Encrypted Value en wachtwoord, hierdoor moet het wachtwoord en het encrypted value juist ingevuld zijn anders krijgt men niets terug
+//       document.getElementById("EncryptedValue").innerHTML,
+//       document.getElementById("password").value
+//     ).toString(CryptoJS.enc.Utf8);// zorgt ervoor dat het een string teruggeeft
+//     document.getElementById("decrypted").innerHTML = decrypted;// het html bestand met het id krijgt de decrypted string terug.
+//     document.getElementById("EncryptedValue").innerHTML = "";//maakt html leeg
+//   }
--- Symmetrische Encryptie/Javascript.js
+++ Symmetrische Encryptie/Javascript.js
@@ -1,55 +1,41 @@
-const getRanHex = size => {
-  let result = [];
-  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f','g', 'h', 'i', 'j', 'k', 'l', 'm','n'];
-
-  for (let n = 0; n < size; n++) {
-    result.push(hexRef[Math.floor(Math.random() * 16)]);
-  }
-  return result.join('');
-}
-
-
+var encryptionkey = CryptoJS.enc.Hex.parse('0123456789abcdef0123456789abcdef')
 const iv = CryptoJS.enc.Hex.parse('fedcba0987654321fedcba0987654321');
 
- // gaat de text ophalen
-
-function encrypt() {
-  var encrypted = CryptoJS.AES.encrypt( //gaat encrypteren op basis van de ingevoerde text en wachtwoord
-    document.getElementById("text").value,
-    document.getElementById("password").value,// haalt het wachtwoord op
-);
-  document.getElementById("EncryptedValue").innerHTML = encrypted; // het html bestand met het id krijgt encrypted ingevuld.
-  document.getElementById("decrypted").innerHTML = ""; //maak decrypted leeg in het geval er iets staat
-}
-
-
-function decrypt() {
-  var decrypted = CryptoJS.AES.decrypt( //gaat decrypteren op basis van de ingevoerde Encrypted Value en wachtwoord, hierdoor moet het wachtwoord en het encrypted value juist ingevuld zijn anders krijgt men niets terug
-    document.getElementById("EncryptedValue").innerHTML,
-    document.getElementById("password").value
-  ).toString(CryptoJS.enc.Utf8);// zorgt ervoor dat het een string teruggeeft
-  document.getElementById("decrypted").innerHTML = decrypted;// het html bestand met het id krijgt de decrypted string terug.
-  document.getElementById("EncryptedValue").innerHTML = "";//maakt html leeg
-}
-
 function iets() {
-  const enc = CryptoJS.AES.encrypt(document.getElementById("text").value, document.getElementById("password").value, getRanHex(32),{
+  var key = getRanHex(32);
+  var msg = document.getElementById("text").value;
+
+  const enc = CryptoJS.AES.encrypt(msg,key,{
     iv,
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
-  console.log(getRanHex(32))
+  console.log(key)
 
-console.log(enc.ciphertext.sigBytes);
 console.log(enc.ciphertext.toString(CryptoJS.enc.Hex));
 }
 
 
 function decryptIets() {
+  var encryptedMessage = document.getElementById("EncryptedValue").innerHTML;
+  var key = document.getElementById("key").innerHTML;
+
+  var decrypt = CryptoJS.AES.decrypt(encryptedMessage,key)
+    .toString(CryptoJS.enc.Utf8)
+  document.getElementById("decrypted").innerHTML = decrypted;
   
 }
 
 
+const getRanHex = size => {
+  let result = [];
+  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  for (let i = 0; i < size; i++) {
+    result.push(hexRef[Math.floor(Math.random() * 16)]);
+  }
+  return result.join('');
+}
 
 
 // wachtwoord, encrypteerd bericht slaan we op in een databank.
