6f4d3856264a67678d7c4a5d0924e6b8ee0a782e
Alec <vanoosterwijckalec@hotmail.com>
2022-04-26 14:14:36 +0200

    laatste aanpassingen

--- Symmetrische Encryptie/Javascript.js
+++ Symmetrische Encryptie/Javascript.js
@@ -1,19 +1,8 @@
-const getRanHex = size => {
-  let result = [];
-  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
-
-  for (let i = 0; i < size; i++) {
-    result.push(hexRef[Math.floor(Math.random() * 16)]);
-  }
-  return result.join('');
-}
-//dit maakt een random 16 bit hexa getal aan. Die we nodig hebben voor een random Encryptie sleutel
-
-var key = CryptoJS.enc.Hex.parse(getRanHex(32));
 const iv = CryptoJS.enc.Hex.parse('fedcba0987654321fedcba0987654321');
 
 function encrypteer() {
-  
+  var key = CryptoJS.enc.Hex.parse(getRanHex(32));
+
   var msg = document.getElementById("text").value;
 
   var enc = CryptoJS.AES.encrypt(msg,key,{
@@ -21,20 +10,43 @@ function encrypteer() {
     mode: CryptoJS.mode.CBC,
     padding: CryptoJS.pad.Pkcs7,
   });
-  console.log(key)
   document.getElementById("EncryptedValue").innerHTML = enc //gaat het geëncypteerd bericht tonen
   document.getElementById("key").innerHTML = key //gaat het encryptiesleutel tonen
 }
 
 function decrypteer() {
   var encryptedMessage = document.getElementById("EncryptedValue").innerHTML;
-  var key = document.getElementById("key").innerHTML;
+  var key = document.getElementById("key").innerHTML; //krijgt een string terug
+  var hexkey = stringToHex(key)
 
-  var decrypt = CryptoJS.AES.decrypt(encryptedMessage,key)
-    .toString(CryptoJS.enc.Utf8) //zet het Geëncrypteerd bericht terug om naar een string
+  var decrypt = CryptoJS.AES.decrypt(encryptedMessage,hexkey);
   document.getElementById("decrypted").innerHTML = decrypt;//toont het bericht
+  console.log(decrypt)
   
 }
+
+const getRanHex = size => {
+  let result = [];
+  let hexRef = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'];
+
+  for (let i = 0; i < size; i++) {
+    result.push(hexRef[Math.floor(Math.random() * 16)]);
+  }
+  console.log(result)
+  return result.join('');
+}
+function stringToHex(key){
+  let result = [];
+
+  for (let i = 0; i < key.length; i++) {
+    result.push(key[i] = " " + key[i]);
+  }
+  console.log(result)
+
+  return result.join('');
+}
+
+//dit maakt een random 16 bit hexa getal aan. Die we nodig hebben voor een random Encryptie sleutel
 
 
 
